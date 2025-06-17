00654acef36e334f8519a97b2505c51b86a28771
ptrst102 <ptrst102@gmail.com>
2025-06-17 16:08:55 +0900

    でぷろい

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-/// <reference types="vitest" />
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/aistoic/",
+  base: "https://ptrst102.github.io/aistoic",
   plugins: [react(), tailwindcss()],
-  test: {
-    environment: "happy-dom",
-    globals: true,
-    setupFiles: "./src/test/setup.ts",
-  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
